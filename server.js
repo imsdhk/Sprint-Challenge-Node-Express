c0624000fadaea8c24569c3948a1e6881b8958b4
Imran Ahmed <jappleleaf@gmail.com>
2018-05-04 12:34:14 -0400

    finished CRUD and retrived the list of actions for a specific project

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+
+const cors = require('cors');
+
+const projectRouter = require('./Routers/RouterProject');
+const actionRouter = require('./Routers/RouterAction');
+
+
+app.use(express.json());
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req,res) => res.send('root route..running'))
+
+app.use('/projects', projectRouter);
+app.use('/actions', actionRouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 5000;
+app.listen(port, () => console.log(`server running on port: ${port}`) )
